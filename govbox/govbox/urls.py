e6a46c694cb1d9a6d65b231f07048c2d9474934c
Amy Zhang <axz@mit.edu>
2020-01-06 10:30:27 -0800

    add custom adminsite

--- govbox/govbox/urls.py
+++ govbox/govbox/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from govrules.admin import admin_site
 
 urlpatterns = [
+    path('', admin_site.urls),
     path('admin/', admin.site.urls),
 ]
