0ff8e536e77b5daac3e35ec1df0d0eee5866d9a9
Amy Zhang <axz@mit.edu>
2020-01-08 22:33:45 -0800

    register

--- govbox/slackintegration/admin.py
+++ govbox/slackintegration/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from govrules.admin import admin_site
-from slackintegration.models import SlackIntegration, SlackUserGroup
+from slackintegration.models import SlackIntegration, SlackUserGroup, UserSignIn
 
 # Register your models here.
 
@@ -12,3 +12,7 @@ admin_site.register(SlackIntegration, SlackIntegrationAdmin)
 class SlackUserGroupAdmin(admin.ModelAdmin):
     pass
 admin_site.register(SlackUserGroup, SlackUserGroupAdmin)
+
+class UserSignInAdmin(admin.ModelAdmin):
+    pass
+admin_site.register(UserSignIn, UserSignInAdmin)
