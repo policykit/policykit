b4339ff7e39545a021c336d9b6c340a905cf90d1
Amy Zhang <axz@mit.edu>
2020-01-08 23:17:20 -0800

    update models

--- govbox/slackintegration/views.py
+++ govbox/slackintegration/views.py
@@ -27,19 +27,18 @@ def oauth(request):
     res = json.loads(resp.read().decode('utf-8'))
     
     if state =="user":
-        if not UserSignIn.objects.filter(access_token=res['access_token']).exists():
-            s = SlackIntegration.objects.filter(team_id=res['team_id'])
-            
-            if s.exists():
-                
-                user_data = parse.urlencode({
+        user_signin = UserSignIn.objects.filter(access_token=res['access_token'])
+        s = SlackIntegration.objects.filter(team_id=res['team_id'])
+        if s.exists():
+            user_data = parse.urlencode({
                 'token': res['access_token']
                 }).encode()
-                
-                user_req = urllib.request.Request('https://slack.com/api/users.identity?', data=user_data)
-                user_resp = urllib.request.urlopen(user_req)
-                user_res = json.loads(user_resp.read().decode('utf-8'))
-                
+            
+            user_req = urllib.request.Request('https://slack.com/api/users.identity?', data=user_data)
+            user_resp = urllib.request.urlopen(user_req)
+            user_res = json.loads(user_resp.read().decode('utf-8'))
+            
+            if not user_signin.exists():           
                 u = UserSignIn.objects.create(
                     slack_team = s[0],
                     user_name = user_res['user']['name'],
@@ -47,15 +46,28 @@ def oauth(request):
                     avatar = user_res['user']['image_24'],
                     access_token=res['access_token']
                     )
+            else:
+                user_signin[0].slack_team = s[0]
+                user_signin[0].user_name = user_res['user']['name']
+                user_signin[0].user_id = user_res['user']['id']
+                user_signin[0].avatar = user_res['user']['image_24']
+                user_signin[0].access_token=res['access_token']
+                user_signin[0].save()
+                
                 
     elif state == "app":
-    
-        if not SlackIntegration.objects.filter(access_token=res['access_token']).exists():
-            s = SlackIntegration.objects.create(
+        s = SlackIntegration.objects.filter(access_token=res['access_token'])
+        if not s.exists():
+            _ = SlackIntegration.objects.create(
                 team_name=res['team_name'],
                 team_id=res['team_id'],
                 access_token=res['access_token']
                 )
+        else:
+            s[0].team_name=res['team_name']
+            s[0].team_id=res['team_id']
+            s[0].access_token=res['access_token']
+            s[0].save()
         
     response = redirect('/')
     return response
