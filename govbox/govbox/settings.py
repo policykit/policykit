8c07e9f5e2c3271a931fcfdc581e69ad7cc1340c
Amy Zhang <axz@mit.edu>
2020-01-12 14:10:42 -0800

    test

--- govbox/govbox/settings.py
+++ govbox/govbox/settings.py
@@ -62,7 +62,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
