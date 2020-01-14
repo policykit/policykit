862ef79e204ca080bd38b2f26b2c9b8156df76cf
Amy Zhang <axz@mit.edu>
2020-01-13 23:32:06 -0800

    passed rules

--- govbox/govrules/admin.py
+++ govbox/govrules/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.admin import AdminSite
-from govrules.models import CommunityIntegration, ProcessMeasure, RuleMeasure, ActionMeasure
+from govrules.models import CommunityIntegration, ProcessMeasure, RuleMeasure, ActionMeasure, Measure
+from django.views.decorators.cache import never_cache
+from django.template.response import TemplateResponse
 
 
 class GovAdminSite(AdminSite):
@@ -12,6 +14,32 @@ class GovAdminSite(AdminSite):
         if request.user.is_anonymous:
             return False
         return True
+    
+    @never_cache
+    def index(self, request, extra_context=None):
+        """
+        Display the main admin index page, which lists all of the installed
+        apps that have been registered in this site.
+        """
+        app_list = self.get_app_list(request)
+        
+        user = request.user
+        community_integration = user.community_integration
+        
+        passed_processes = ProcessMeasure.objects.filter(status=Measure.PASSED, community_integration=community_integration)
+
+        passed_rules = RuleMeasure.objects.filter(status=Measure.PASSED, community_integration=community_integration)
+
+        context = {**self.each_context(request), 
+                   'title': self.index_title, 
+                   'app_list': app_list, 
+                   'passed_processes': passed_processes,
+                   'passed_rules': passed_rules,
+                   **(extra_context or {})}
+
+        request.current_app = self.name
+
+        return TemplateResponse(request, self.index_template or 'admin/index.html', context)
 
 
 admin_site = GovAdminSite(name="govadmin")
