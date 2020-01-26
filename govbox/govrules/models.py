945802e75215723ef8787d2a9469f327fd0edf12
Amy Zhang <axz@mit.edu>
2020-01-26 13:54:05 -0800

    add time proposed

--- govbox/govrules/models.py
+++ govbox/govrules/models.py
@@ -79,6 +79,8 @@ class Measure(PolymorphicModel):
         blank=True
     )
     
+    proposal_time = models.DateTimeFiled(auto_now_add=True)
+    
     PROPOSED = 'proposed'
     FAILED = 'failed'
     PASSED = 'passed'
