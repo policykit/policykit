27e8a32b92a0dc78e2604e6c15e10b65ea1de0aa
Amy Zhang <axz@mit.edu>
2020-01-26 14:40:32 -0800

    fix

--- govbox/govrules/tasks.py
+++ govbox/govrules/tasks.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 
 from celery import shared_task
 from celery.schedules import crontab
-from govrules.models import UserVote, ActionMeasure, Measure, RuleMeasure
+from govrules.models import *
 from govbox.celery import app
 from govrules.views import *
 
