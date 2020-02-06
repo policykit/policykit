ad41b43d3c661664bace67093ab12506ce838723
Amy Zhang <axz@mit.edu>
2020-02-05 22:13:38 -0800

    fix

--- policykit/slackintegration/views.py
+++ policykit/slackintegration/views.py
@@ -92,14 +92,15 @@ def action(request):
             new_action.save(slack_revert=True, inviter=inviter_user)
     
         elif event.get('type') == 'message':
-            new_action = SlackPostMessage()
-            new_action.community_integration = integration
-            new_action.author = author
-            new_action.text = event['text']
-            new_action.channel = event['channel']
-            time_stamp = event['ts']
-            poster = event['user']
-            new_action.save(time_stamp=time_stamp, poster=poster)
+            if event.get('subtype') == None:
+                new_action = SlackPostMessage()
+                new_action.community_integration = integration
+                new_action.author = author
+                new_action.text = event['text']
+                new_action.channel = event['channel']
+                time_stamp = event['ts']
+                poster = event['user']
+                new_action.save(time_stamp=time_stamp, poster=poster)
             
     
     return HttpResponse("")
