fcc7f40971b74f3d40503e6509f3bb7e09bcd291
Grant Hugh <granthugh@DNa810956.SUNet>
2020-02-06 22:51:01 -0800

    fix

--- policykit/slackintegration/views.py
+++ policykit/slackintegration/views.py
@@ -113,7 +113,8 @@ def action(request):
             new_action = SlackPinMessage()
             new_action.community_integration = integration
             new_action.author = author
-            new_action.channel = event['channel']['id']
+            new_action.channel = event['channel_id']
+            # also have 'event_ts' field for timestamp
             new_action.save()
             
         elif event.get('type') == 'reaction_added':
