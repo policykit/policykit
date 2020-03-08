29f51ec4ebd9c2acd255087614da3d8734319c12
Amy Zhang <axz@mit.edu>
2020-03-08 16:53:10 -0700

    fix

--- policykit/slackintegration/auth_backends.py
+++ policykit/slackintegration/auth_backends.py
@@ -36,16 +36,24 @@ class SlackBackend(BaseBackend):
                 user_resp = urllib.request.urlopen(user_req)
                 user_res = json.loads(user_resp.read().decode('utf-8'))
 
-                slack_user,_ = SlackUser.objects.get_or_create(
-                    username=oauth['authed_user']['id'],
-                    password=oauth['authed_user']['access_token'],
-                    community_integration = s[0],
-                    readable_name = user_res['user']['name'],
-                    avatar = user_res['user']['image_24'],
-                    access_token = oauth['authed_user']['access_token'],
-                    )
-                                
-                slack_user.save()
+
+                if slack_user.exists():
+                    slack_user = slack_user[0]
+                    slack_user.access_token = oauth['authed_user']['access_token']
+                    slack_user.community_integration = s[0]
+                    slack_user.password = oauth['authed_user']['access_token']
+                    slack_user.readable_name = user_res['user']['name']
+                    slack_user.avatar = user_res['user']['image_24']
+                    slack_user.save()
+                else:
+                    slack_user,_ = SlackUser.objects.get_or_create(
+                        username=oauth['authed_user']['id'],
+                        password=oauth['authed_user']['access_token'],
+                        community_integration = s[0],
+                        readable_name = user_res['user']['name'],
+                        avatar = user_res['user']['image_24'],
+                        access_token = oauth['authed_user']['access_token'],
+                        )
             return slack_user
         return None
 
