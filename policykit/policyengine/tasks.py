c7897baba78fd94544537b050bd0c25c16466e55
Amy Zhang <axz@mit.edu>
2020-03-09 09:09:06 -0700

    fix

--- policykit/policyengine/tasks.py
+++ policykit/policyengine/tasks.py
@@ -3,18 +3,23 @@ from __future__ import absolute_import, unicode_literals
 
 from celery import shared_task
 from celery.schedules import crontab
-from policyengine.models import UserVote, CommunityAction, Proposal, CommunityPolicy, CommunityUser
+from policyengine.models import UserVote, CommunityAction, CommunityActionBundle, Proposal, CommunityPolicy, CommunityUser
 from policykit.celery import app
 from policyengine.views import *
 
 @shared_task
 def consider_proposed_actions():
-    proposed_actions = CommunityAction.objects.filter(proposal__status=Proposal.PROPOSED, is_bundled=False)
+    community_actions = CommunityAction.objects.filter(proposal__status=Proposal.PROPOSED, is_bundled=False)
+    bundle_actions = CommunityActionBundle.objects.filter(proposal__status=Proposal.PROPOSED)
+    proposed_actions = community_actions + bundle_actions
+    
     for action in proposed_actions:
         for policy in CommunityPolicy.objects.filter(proposal__status=Proposal.PASSED, community_integration=action.community_integration):
+            
             if check_filter_code(policy, action):
                 cond_result = check_policy_code(policy, action)
                 if cond_result == Proposal.PASSED:
+                    
                     exec(policy.policy_action_code)
                 elif cond_result == Proposal.FAILED:
                     exec(policy.policy_failure_code)
