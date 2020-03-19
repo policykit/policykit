89f05a327a6df66253e93b86c7eff217da3a5f58
Amy Zhang <axz@mit.edu>
2020-03-19 19:43:02 -0400

    try

--- policykit/policykit/settings.py
+++ policykit/policykit/settings.py
@@ -75,6 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.template.context_processors.i18n',
+                'django.template.context_processors.tz',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
--- policykit/policykit/urls.py
+++ policykit/policykit/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, url
 from django.conf.urls import include
 from policyengine.admin import admin_site
 
