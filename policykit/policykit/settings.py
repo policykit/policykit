aefee575ec8e73976f85d03f595df89a180dc1e9
Amy Zhang <axz@mit.edu>
2020-03-19 19:36:11 -0400

    fix

--- policykit/policykit/settings.py
+++ policykit/policykit/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.i18n',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
