c357432fde1c736f867267cbd18fb5fd1b0e3e6a
Amy Zhang <axz@mit.edu>
2020-03-19 19:05:46 -0400

    fix

--- policykit/policykit/settings.py
+++ policykit/policykit/settings.py
@@ -186,3 +186,38 @@ CELERY_BEAT_SCHEDULE = {
 }
 
 
+JET_DEFAULT_THEME = 'light-gray'
+
+JET_THEMES = [
+    {
+    'theme': 'default', # theme folder name
+    'color': '#47bac1', # color of the theme's button in user menu
+    'title': 'Default' # theme title
+    },
+    {
+    'theme': 'green',
+    'color': '#44b78b',
+    'title': 'Green'
+    },
+    {
+    'theme': 'light-green',
+    'color': '#2faa60',
+    'title': 'Light Green'
+    },
+    {
+    'theme': 'light-violet',
+    'color': '#a464c4',
+    'title': 'Light Violet'
+    },
+    {
+    'theme': 'light-blue',
+    'color': '#5EADDE',
+    'title': 'Light Blue'
+    },
+    {
+    'theme': 'light-gray',
+    'color': '#222',
+    'title': 'Light Gray'
+    }
+]
+
