bf64ee52cdfac649865c9e859e105d81582dc448
Amy Zhang <axz@mit.edu>
2020-03-19 23:08:14 -0400

    fix

--- policykit/policykit/dashboard_modules.py
+++ policykit/policykit/dashboard_modules.py
@@ -4,18 +4,20 @@ from policyengine.models import CommunityPolicy, Proposal
 
 class CommunityPolicyModule(DashboardModule):
     
-    ajax_load = True
+    title = _('Passed Community Policies')
     
     template = 'policyadmin/dashboard_modules/community_policy.html'
+    
+    layout = 'stacked'
+    
+    children = []
     draggable = False
     collapsible = True
     deletable = False
     show_title = True
-    title = 'Passed Community Policies'
-    title_url = None
-
+    
+    
     def init_with_context(self, context):
-        
         passed_community_policies = CommunityPolicy.objects.filter(proposal__status=Proposal.PASSED, 
                                                                    community_integration=context['request'].user.community_integration)
         for i in passed_community_policies:
@@ -23,6 +25,6 @@ class CommunityPolicyModule(DashboardModule):
             if c.exists():
                 c = c[0]
                 i.bundle = c
-                
-        self.children =  passed_community_policies
+            self.children.append({'is_bundled': i.is_bundled,
+                                  'id': i.id})
         
