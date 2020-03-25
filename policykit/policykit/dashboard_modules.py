af33aae32620e88f0a7eefbd15faa15d406febdf
Amy Zhang <axz@mit.edu>
2020-03-24 23:36:05 -0400

    fix

--- policykit/policykit/dashboard_modules.py
+++ policykit/policykit/dashboard_modules.py
@@ -30,7 +30,7 @@ class RolePermissionModule(DashboardModule):
                 role_info['permissions'].append({'name': p.name})
                 
             for u in i.user_set.all():
-                role_info['users'].append({'username': u.readable_name})
+                role_info['users'].append({'username': u.communityuser.readable_name})
             
             self.children.append(role_info)
         
