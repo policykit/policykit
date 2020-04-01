9875dccf1de80a829943a63fc9e1d817f97d990b
Amy Zhang <axz@mit.edu>
2020-04-01 12:15:55 -0400

    fix

--- policykit/policyengine/views.py
+++ policykit/policyengine/views.py
@@ -46,7 +46,7 @@ def check_policy_code(policy, action):
     
     _locals = locals()
     
-    wrapper_start = "def check():\r\n  global PASSED'\r\n"
+    wrapper_start = "def check():\r\n  global PASSED\r\n"
     
     wrapper_end = "\r\npolicy_pass = check()"
      
