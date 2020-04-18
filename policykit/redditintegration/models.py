a3564797c637fc4e428168ebce6c6a864cc601a6
Amy Zhang <axz@mit.edu>
2020-04-17 21:53:03 -0400

    fix

--- policykit/redditintegration/models.py
+++ policykit/redditintegration/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from policyengine.models import Community, CommunityUser, CommunityAction
+from django.contrib.auth.models import Permission, ContentType, User
+from policykit.settings import REDDIT_CLIENT_SECRET
+import urllib
+from urllib import parse
+import base64
+import json
 
 # Create your models here.
+
+
+def refresh_token(refresh_token):
+    data = parse.urlencode({
+        'grant_type': 'refresh_token',
+        'refresh_token': refresh_token
+        }).encode()
+        
+    req = urllib.request.Request('https://www.reddit.com/api/v1/access_token', data=data)
+    
+    credentials = ('%s:%s' % ('QrZzzkLgVc1x6w', REDDIT_CLIENT_SECRET))
+    encoded_credentials = base64.b64encode(credentials.encode('ascii'))
+
+    req.add_header("Authorization", "Basic %s" % encoded_credentials.decode("ascii"))
+    req.add_header("User-Agent", "PolicyKit-App-Reddit-Integration v 1.0")
+
+    resp = urllib.request.urlopen(req)
+    res = json.loads(resp.read().decode('utf-8'))
+    return res
+
+
+class RedditCommunity(Community):
+    API = 'https://reddit.com/api/v1/'
+    
+    team_id = models.CharField('team_id', max_length=150, unique=True)
+
+    access_token = models.CharField('access_token', 
+                                    max_length=300, 
+                                    unique=True)
+    
+    refresh_token = models.CharField('refresh_token', 
+                               max_length=500, 
+                               null=True)
+    
+    
+    def refresh_token(self):
+        res = refresh_token(self.refresh_token)
+        self.access_token = res['access_token']
+        self.save()
+
+    
+    def notify_action(self, action, policy, users, post_type='channel', template=None, channel=None):
+        from redditintegration.views import post_policy
+        post_policy(policy, action, users, post_type, template, channel)
+    
+    def save(self, *args, **kwargs):      
+        super(RedditCommunity, self).save(*args, **kwargs)
+        
+#         content_types = ContentType.objects.filter(model__in=REDDIT_ACTIONS)
+#         perms = Permission.objects.filter(content_type__in=content_types, name__contains="can add ")
+#         for p in perms:
+#             self.base_role.permissions.add(p)
+            
+
+class RedditUser(CommunityUser):
+    refresh_token = models.CharField('refresh_token', 
+                               max_length=500, 
+                               null=True)
+    
+    def refresh_token(self):
+        res = refresh_token(self.refresh_token)
+        self.access_token = res['access_token']
+        self.save()
+    
+    def save(self, *args, **kwargs):      
+        super(RedditUser, self).save(*args, **kwargs)
+        group = self.community.base_role
+        group.user_set.add(self)
+
+
+
--- policykit/redditintegration/views.py
+++ policykit/redditintegration/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from policykit.settings import REDDIT_CLIENT_SECRET
 from django.shortcuts import redirect
+from redditintegration.models import RedditCommunity, RedditUser
+from policyengine.models import *
 from django.views.decorators.csrf import csrf_exempt
 from urllib import parse
 import urllib.request
@@ -43,6 +45,49 @@ def oauth(request):
         
         logger.info(res)
         
+        s = RedditCommunity.objects.filter(team_id=res['team']['id'])
+            community = None
+            user_group,_ = CommunityRole.objects.get_or_create(name="Base User")
+            if not s.exists():
+                community = SlackCommunity.objects.create(
+                    community_name=res['team']['name'],
+                    team_id=res['team']['id'],
+                    access_token=res['access_token'],
+                    base_role=user_group
+                    )
+                user_group.community = community
+                user_group.save()
+                
+                cg = CommunityDoc.objects.create(text='',
+                                                 community=community)
+                
+                
+                community.community_guidelines=cg
+                community.save()
+                
+            else:
+                s[0].community_name = res['team']['name']
+                s[0].team_id = res['team']['id']
+                s[0].access_token = res['access_token']
+                s[0].save()
+                community = s[0]
+            
+            user = SlackUser.objects.filter(username=res['authed_user']['id'])
+            if not user.exists():
+                
+                # CHECK HERE THAT USER IS ADMIN
+                
+                _ = SlackUser.objects.create(username=res['authed_user']['id'],
+                                             access_token=res['authed_user']['access_token'],
+                                             is_community_admin=True,
+                                             community=community
+                                             )
+        
+        
+        
+        
+        
+        
     
         response = redirect('/login?success=true')
         return response
@@ -58,3 +103,6 @@ def action(request):
     logger.info('RECEIVED ACTION')
     logger.info(json_data)
     
+    
+def post_policy():
+    pass
