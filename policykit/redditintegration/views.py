a5b2f6f3f3ba669ad7a0b903bfe5cb9388ee9aad
rickygv99 <rickygv99@yahoo.com>
2020-06-29 19:45:27 -0700

    bugfix

--- policykit/redditintegration/views.py
+++ policykit/redditintegration/views.py
@@ -102,7 +102,7 @@ def oauth(request):
 
             logger.info(community.access_token)"""
 
-            response = redirect('/reddit/configure?subreddits=' + ','.join(titles))
+            response = redirect('/reddit?subreddits=' + ','.join(titles))
             return response
 
     response = redirect('/login?error=no_subreddits_with_mod_privileges_found')
