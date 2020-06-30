0f6809677276e9bc21576b85cef47306c5cb9506
rickygv99 <rickygv99@yahoo.com>
2020-06-29 20:13:31 -0700

    bugfix

--- policykit/redditintegration/views.py
+++ policykit/redditintegration/views.py
@@ -102,7 +102,7 @@ def oauth(request):
 
             logger.info(community.access_token)"""
 
-            encoded_titles = urllib.urlencode(titles)
+            encoded_titles = parse.urlencode(titles)
             response = redirect('/configure?%s'%encoded_titles)
             return response
 
