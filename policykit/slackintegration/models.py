3d26606b0bd6e97ad87d4eaaf6eb7e849b9eb598
Evan Kim <evankim@macbook-pro-24.lan>
2020-07-02 15:03:45 -0700

    test

--- policykit/slackintegration/models.py
+++ policykit/slackintegration/models.py
@@ -24,7 +24,7 @@ class SlackCommunity(Community):
                                     max_length=300, 
                                     unique=True)
     
-    bot_id = models.CharField('bot_id', max_length=150, unique=True, default=None, null=True, blank=True)
+    bot_id = models.CharField('bot_id', max_length=150, unique=True, default='')
     
     def notify_action(self, action, policy, users, post_type='channel', template=None, channel=None):
         from slackintegration.views import post_policy
