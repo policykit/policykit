be8af81a928adff10132ad880288c62be5eb3f84
Amy Zhang <amy.xian.zhang@gmail.com>
2020-07-06 14:21:53 -0400

    Update settings.py

--- policykit/policykit/settings.py
+++ policykit/policykit/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ['policykit.org',
                  'ec2-54-189-150-51.us-west-2.compute.amazonaws.com',
                  '54.189.150.51',
                  '127.0.0.1',
-                 'ec2-18-237-195-24.us-west-2.compute.amazonaws.com']
+                 'ec2-34-222-165-120.us-west-2.compute.amazonaws.com']
 
 
 # Application definition
