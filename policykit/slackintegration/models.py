ad447a83cb8fbc03dc6fdfc7e9bd5cc47d3d5e9e
Evan Kim <evankim@macbook-pro-24.lan>
2020-07-08 15:27:23 -0700

    test

--- policykit/slackintegration/models.py
+++ policykit/slackintegration/models.py
@@ -48,10 +48,10 @@ class SlackCommunity(Community):
     def save(self, *args, **kwargs):      
         super(SlackCommunity, self).save(*args, **kwargs)
         
-        content_types = ContentType.objects.filter(model__in=SLACK_ACTIONS)
-        perms = Permission.objects.filter(content_type__in=content_types, name__contains="can add ")
-        for p in perms:
-            self.base_role.permissions.add(p)
+        #content_types = ContentType.objects.filter(model__in=SLACK_ACTIONS)
+        #perms = Permission.objects.filter(content_type__in=content_types, name__contains="can add ")
+        #for p in perms:
+        #    self.base_role.permissions.add(p)
             
             
     def execute_community_action(self, action, delete_policykit_post=True):
