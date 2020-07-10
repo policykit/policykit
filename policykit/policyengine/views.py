91ebbc7451a6a035de9b38863a021f93c202b200
rickygv99 <rickygv99@yahoo.com>
2020-07-09 20:08:33 -0700

    Added wrappers to user code functions + decomposed local exec

--- policykit/policyengine/views.py
+++ policykit/policyengine/views.py
@@ -7,69 +7,83 @@ import logging
 import json
 
 
-
 logger = logging.getLogger(__name__)
 
+def exec_code(code, wrapperStart, wrapperEnd, globals, locals):
+    lines = ['  ' + item for item in code.splitlines()]
+    code = wrapperStart + '\r\n'.join(lines) + wrapperEnd
 
-def check_filter_code(policy, action):
+    exec(code, globals, locals)
+
+def filter_policy(policy, action):
     _locals = locals()
-     
+
     wrapper_start = "def filter():\r\n"
+
     wrapper_end = "\r\nfilter_pass = filter()"
-     
-    lines = ['  ' + item for item in policy.policy_filter_code.splitlines()]
-    filter_str = '\r\n'.join(lines)
-    filter_code = wrapper_start + filter_str + wrapper_end
-     
-     
-    exec(filter_code, None, _locals)
-    
+
+    exec_code(policy.policy_filter_code, wrapper_start, wrapper_end, None, _locals)
+
     if _locals.get('filter_pass'):
         return _locals['filter_pass']
     else:
         return False
 
+def initialize_policy(policy, action):
+    wrapper_start = "def initialize():\r\n"
 
+    wrapper_end = "\r\ninitialize()"
 
-def initialize_code(policy, action):
-    exec(policy.policy_init_code, globals(), locals())
-    
+    exec_code(policy.policy_init_code, wrapper_start, wrapper_end, globals(), locals())
+
     policy.has_notified = True
     policy.save()
-    
 
-
-def check_policy_code(policy, action):
+def check_policy(policy, action):
     from policyengine.models import Proposal, CommunityUser, BooleanVote, NumberVote
 
     users = CommunityUser.objects.filter(community=policy.community)
     boolean_votes = BooleanVote.objects.filter(proposal=action.proposal)
     number_votes = NumberVote.objects.filter(proposal=action.proposal)
-    
+
     _locals = locals()
-    
+
     wrapper_start = "def check(policy, action, users, boolean_votes, number_votes):\r\n"
     wrapper_start += "  PASSED = 'passed'\r\n  FAILED = 'failed'\r\n  PROPOSED = 'proposed'\r\n"
-    
-    wrapper_end = "\r\npolicy_pass = check(policy, action, users, boolean_votes, number_votes)"
-     
-    lines = ['  ' + item for item in policy.policy_conditional_code.splitlines()]
-    check_str = '\r\n'.join(lines)
-    check_code = wrapper_start + check_str + wrapper_end
 
-    exec(check_code, None, _locals)
-    
+    wrapper_end = "\r\npolicy_pass = check(policy, action, users, boolean_votes, number_votes)"
+
+    exec_code(policy.policy_conditional_code, wrapper_start, wrapper_end, None, _locals)
+
     if _locals.get('policy_pass'):
         return _locals['policy_pass']
     else:
         return Proposal.PROPOSED
 
+def notify_policy(policy, action):
+    wrapper_start = "def notify():\r\n"
 
+    wrapper_end = "\r\nnotify()"
 
+    exec_code(policy.policy_notify_code, wrapper_start, wrapper_end, None, None)
+
+def pass_policy(policy, action):
+    wrapper_start = "def success():\r\n"
+
+    wrapper_end = "\r\nsuccess()"
+
+    exec_code(policy.policy_action_code, wrapper_start, wrapper_end, None, None)
+
+def fail_policy(policy, action):
+    wrapper_start = "def fail():\r\n"
+
+    wrapper_end = "\r\nfail()"
+
+    exec_code(policy.policy_failure_code, wrapper_start, wrapper_end, None, None)
 
 def clean_up_proposals(action, executed):
     from policyengine.models import Proposal, CommunityActionBundle
-    
+
     if action.is_bundled:
         bundle = action.communityactionbundle_set.all()
         if bundle.exists():
@@ -87,15 +101,10 @@ def clean_up_proposals(action, executed):
             else:
                 p.status = Proposal.FAILED
             p.save()
-            
-        
+
     p = action.proposal
     if executed:
         p.status = Proposal.PASSED
     else:
         p.status = Proposal.FAILED
     p.save()
-            
-            
-            
-    
