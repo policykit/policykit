9fcf16bf496335fbb120fb41498e84f9010dc1db
rickygv99 <rickygv99@yahoo.com>
2020-07-12 23:41:32 -0700

    bugfix

--- policykit/policyengine/tests.py
+++ policykit/policyengine/tests.py
@@ -6,10 +6,10 @@ def filter_test(code, shouldPass=True):
     try:
         filter_code(code)
         if shouldPass == False:
-            print("Test case failed: " + e)
-    except NonWhitelistedCodeError as e:
+            print("FAILED test case: should have passed: " + code + "\n")
+    except NonWhitelistedCodeError:
         if shouldPass:
-            print("Test case failed: " + e)
+            print("FAILED test case: should NOT have passed: " + code + "\n")
 
 dangerous_modules = [
     "os",
@@ -35,19 +35,19 @@ dangerous_functions = [
 # Create your tests here.
 class FilterTests(TestCase):
     def test_import_whitelisted_modules(self):
-        print("Testing importing of whitelisted modules")
+        print("Testing importing of whitelisted modules\n")
         for module in whitelisted_modules:
             code = "import " + module
             filter_test(code)
 
     def test_dangerous_modules(self):
-        print("Testing importing of dangerous modules")
+        print("Testing importing of dangerous modules\n")
         for module in dangerous_modules:
             code = "import " + module
             filter_test(code)
 
     def test_dangerous_functions(self):
-        print("Testing calling of dangerous functions")
+        print("Testing calling of dangerous functions\n")
         for function in dangerous_functions:
             code = function + "()"
             filter_test(code)
