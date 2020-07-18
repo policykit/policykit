30261a11762cf6898d7ee409706c4e2904420277
rickygv99 <rickygv99@yahoo.com>
2020-07-17 22:55:45 -0700

    bugfix

--- policykit/discordintegration/views.py
+++ policykit/discordintegration/views.py
@@ -85,7 +85,7 @@ def oauth(request):
             # Get the list of users and create a DiscordUser object for each user
             req = urllib.request.Request('https://discordapp.com/api/guilds/%s/members' % guild_id)
             req.add_header("Content-Type", "application/json")
-            req.add_header('Authorization', 'Bearer %s' % res['access_token'])
+            req.add_header('Authorization', 'Bot %s' % DISCORD_BOT_TOKEN)
             req.add_header("User-Agent", "Mozilla/5.0") # yes, this is strange. discord requires it when using urllib for some weird reason
             resp = urllib.request.urlopen(req)
             guild_members = json.loads(resp.read().decode('utf-8'))
