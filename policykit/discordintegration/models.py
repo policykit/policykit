a6d66b17d63e53957720bdf0414d98b6b555833c
ekim6767 <ekim67@stanford.edu>
2020-07-30 10:41:57 -0700

    rename fix

--- policykit/discordintegration/models.py
+++ policykit/discordintegration/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from policyengine.models import Community, CommunityUser, CommunityAction
+from policyengine.models import Community, CommunityUser, PlatformAction
 from django.contrib.auth.models import Permission, ContentType, User
 from policykit.settings import DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, DISCORD_BOT_TOKEN
 import urllib
