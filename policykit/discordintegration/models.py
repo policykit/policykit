59ac2e4bc56a899058064340dfec9f3f1dbc534a
ekim6767 <ekim67@stanford.edu>
2020-07-30 10:43:02 -0700

    rename fix2

--- policykit/discordintegration/models.py
+++ policykit/discordintegration/models.py
@@ -157,7 +157,7 @@ class DiscordUser(CommunityUser):
         group = self.community.base_role
         group.user_set.add(self)
 
-class DiscordPostMessage(CommunityAction):
+class DiscordPostMessage(PlatformAction):
 
     guild_id = None
     id = None
