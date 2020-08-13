1f1f0f1caaa5b90e457cc4c317825fc16c64b5f9
Sreya Guha <66488292+sreyaguha1@users.noreply.github.com>
2020-08-12 23:49:04 -0700

    new

--- policykit/policyengine/models.py
+++ policykit/policyengine/models.py
@@ -1,7 +1,6 @@
 from django.db import models, transaction
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django import forms
 from django.contrib.auth.models import User, Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
