04b3ce04b391ef89277ef0b843c07d8373d08c2b
ekim6767 <ekim67@stanford.edu>
2020-08-12 17:38:03 -0700

    test

--- policykit/slackintegration/models.py
+++ policykit/slackintegration/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from policyengine.models import Community, CommunityUser, PlatformAction, StarterKit
+from policyengine.models import Community, CommunityUser, PlatformAction, StarterKit, ConstitutionPolicy, Proposal, PlatformPolicy, CommunityRole
 from django.contrib.auth.models import Permission, ContentType, User
 import urllib
 import json
