15b30cc4cca887e0917616851c951c05f66bf5a6
ekim6767 <ekim67@stanford.edu>
2020-08-12 18:58:42 -0700

    test

--- policykit/policyengine/models.py
+++ policykit/policyengine/models.py
@@ -4,7 +4,7 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User, Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.postgres.fields import ArrayField
+#from django.contrib.postgres.fields import ArrayField
 # from django.contrib.govinterface.models import LogEntry
 from polymorphic.models import PolymorphicModel
 from django.core.exceptions import ValidationError
