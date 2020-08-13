7ecbc352409e31606b675d29a343c03abca37dd8
Sreya Guha <66488292+sreyaguha1@users.noreply.github.com>
2020-08-12 23:36:48 -0700

    new

--- policykit/policyengine/models.py
+++ policykit/policyengine/models.py
@@ -851,7 +851,7 @@ class BooleanVote(UserVote):
                           (False, 'No')
                           )
     boolean_value = models.BooleanField(null = True, choices = TRUE_FALSE_CHOICES,
-                                                               initial='', widget=forms.Select())) # yes/no, selected/not selected
+                                                               initial='', widget=forms.Select()) # yes/no, selected/not selected
 
 
 class NumberVote(UserVote):
