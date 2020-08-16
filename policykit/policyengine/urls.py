c2933c44052e929e45e729d1bf5c3c0e7a7c37fd
rickygv99 <rickygv99@yahoo.com>
2020-08-15 21:02:13 -0700

    bugfix

--- policykit/policyengine/urls.py
+++ policykit/policyengine/urls.py
@@ -5,5 +5,5 @@ from policyengine import views
 urlpatterns = [
     path('initialize_starterkit', views.initialize_starterkit),
     path('error_check', views.error_check),
-    path('save_policy', views.policy_action_save)
+    path('policy_action_save', views.policy_action_save)
 ]
