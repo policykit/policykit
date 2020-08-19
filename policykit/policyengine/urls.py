9f946b578b3d5c04177b80a33f99acab18413200
Sreya Guha <66488292+sreyaguha1@users.noreply.github.com>
2020-08-19 10:00:49 -0700

    trying activity stream pls work (removing RSS)

--- policykit/policyengine/urls.py
+++ policykit/policyengine/urls.py
@@ -6,6 +6,5 @@ from .feeds import LatestPostsFeed
 urlpatterns = [
     path('initialize_starterkit', views.initialize_starterkit),
     path('error_check', views.error_check),
-    path('policy_action_save', views.policy_action_save),
-    path('feed/rss', LatestPostsFeed(), name='post_feed')
+    path('policy_action_save', views.policy_action_save)
 ]
