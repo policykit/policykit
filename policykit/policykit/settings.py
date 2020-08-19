9590df659f33679fa3ef93a4604fe4d78b5cba6c
Sreya Guha <66488292+sreyaguha1@users.noreply.github.com>
2020-08-19 09:32:33 -0700

    trying activity stream pls work

--- policykit/policykit/settings.py
+++ policykit/policykit/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
     'django.contrib.staticfiles',
     'django_celery_beat',
     'django_celery_results',
