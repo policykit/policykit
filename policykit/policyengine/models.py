a87dfd3705010d5d77b3704c49304c8d2db2bdd5
Sreya Guha <66488292+sreyaguha1@users.noreply.github.com>
2020-08-25 02:13:28 -0700

    sync

--- policykit/policyengine/models.py
+++ policykit/policyengine/models.py
@@ -57,8 +57,9 @@ class Community(PolymorphicModel):
     def notify_action(self, action, policy, users):
         pass
 
-
-
+    def get_users(self, users):
+        return users
+#users = CommunityUser.objects.filter(community=user.community)
 
 
 
