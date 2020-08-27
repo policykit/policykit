1c6e821c96a2f650a8d1db328a44cee64c1fcf4f
rickygv99 <rickygv99@yahoo.com>
2020-08-27 01:34:42 -0700

    fix permissions

--- policykit/policyengine/views.py
+++ policykit/policyengine/views.py
@@ -500,24 +500,16 @@ def role_action_save(request):
 
 @csrf_exempt
 def document_action_save(request):
-    logger.info('entered')
     from policyengine.models import PolicykitAddCommunityDoc
 
     data = json.loads(request.body)
     user = get_user(request)
 
-    logger.info('about to create')
-
     action = PolicykitAddCommunityDoc()
     action.community = user.community
     action.initiator = user
-    logger.info('user')
     action.name = data['name']
-    logger.info('name' + action.name)
     action.text = data['text']
-    logger.info('text' + action.text)
     action.save()
-
-    logger.info('saved successfully')
 
     return HttpResponse()
