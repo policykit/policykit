192439c3e0a264577029302bdfc947519db52ac8
rickygv99 <rickygv99@yahoo.com>
2021-01-27 18:52:39 -0800

    test

--- policykit/integrations/discourse/views.py
+++ policykit/integrations/discourse/views.py
@@ -65,7 +65,7 @@ def init_community(request):
     private_key = RSA.importKey(request.session['private_key'])
 
     payload_encrypted = request.GET['payload']
-    payload = private_key.decrypt(base64.b64decode(payload_encrypted)).decode()
+    payload = private_key.decrypt(base64.b64decode(payload_encrypted)).decode('utf-8', 'ignore')
     payload_body = api_key[api_key.index('{'):] # Removes gobbledy-gook heading and returns json string
     payload_body_json = json.loads(payload_body)
     api_key = payload_body_json['key']
