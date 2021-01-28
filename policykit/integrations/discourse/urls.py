a37db938fac49f98fbf79758f2d2d98437a4f276
rickygv99 <rickygv99@yahoo.com>
2021-01-27 22:15:37 -0800

    bugfix

--- policykit/integrations/discourse/urls.py
+++ policykit/integrations/discourse/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('action', views.action),
     path('configure', views.configure),
     path('auth', views.auth),
-    path('user_login', views.user_login),
     path('init_community', views.init_community)
 ]
