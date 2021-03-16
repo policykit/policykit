eac69eb5f77594f3e9e5e29e6c0301621ce20615
Miriam Ashton <miriamashton3@gmail.com>
2021-03-16 11:23:34 -0400

    Update policy filter to match new function names

--- policykit/policyengine/tests/test_filter.py
+++ policykit/policyengine/tests/test_filter.py
@@ -5,10 +5,10 @@ def filter_test(code, shouldPass=True):
     errors = filter_code(code)
     if len(errors) > 0:
         if shouldPass:
-            print("FAILED test case: should have passed: " + code + "\n")
+            raise Exception("FAILED test case: should have passed: " + code + "\n")
     else:
         if shouldPass == False:
-            print("FAILED test case: should NOT have passed: " + code + "\n")
+            raise Exception("FAILED test case: should NOT have passed: " + code + "\n")
 
 dangerous_modules = [
     "os",
@@ -52,7 +52,7 @@ voter_users = users.filter(groups__name__in=['Moderator'])
 yes_votes = action.proposal.get_yes_votes(users=voter_users, value=True)
 if len(yes_votes) >= math.ceil(voter_users.count()/2):
     return PASSED
-elif action.proposal.time_elapsed() > datetime.timedelta(days=1):
+elif action.proposal.get_time_elapsed() > datetime.timedelta(days=1):
     return FAILED
 """,
 
@@ -70,7 +70,7 @@ jury_users = users.filter(username__in=jury)
 yes_votes = action.proposal.get_yes_votes(users=jury_users, value=True)
 if len(yes_votes) >= 2:
     return PASSED
-elif action.proposal.time_elapsed() > datetime.timedelta(days=2):
+elif action.proposal.get_time_elapsed() > datetime.timedelta(days=2):
     return FAILED
 """,
 ]
