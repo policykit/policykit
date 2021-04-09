f8873659a52f79fb0a69bb26d6be058515c56379
rickygv99 <rickygv99@yahoo.com>
2021-04-09 00:18:02 -0700

    updates

--- policykit/integrations/metagov/views.py
+++ policykit/integrations/metagov/views.py
@@ -1,15 +1,70 @@
 import json
 import logging
 
-from django.http import HttpResponse, HttpResponseBadRequest
+import requests
+from django.conf import settings
+from django.contrib.auth import get_user
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import ContentType, Permission
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import PermissionDenied
+from django.http import (HttpResponse, HttpResponseBadRequest,
+                         HttpResponseServerError)
+from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from integrations.metagov.models import ExternalProcess
+from integrations.metagov.library import metagov_slug, update_metagov_community
+from integrations.metagov.models import (ExternalProcess,
+                                         MetagovPlatformAction, MetagovUser)
+from policyengine.models import Community, CommunityRole
 
 logger = logging.getLogger(__name__)
 
+
+@login_required(login_url="/login")
+def config_editor(request):
+    user = get_user(request)
+    if not user.is_community_admin:
+        raise PermissionDenied
+
+    community = user.community
+    url = f"{settings.METAGOV_URL}/api/internal/community/{metagov_slug(community)}"
+    response = requests.get(url)
+    if not response.ok:
+        if response.status_code == 404:
+            logger.info(f"No metagov community for {community.community_name}, creating for the first time")
+            config = update_metagov_community(community)
+        else:
+            raise Exception(response.text)
+    else:
+        config = response.json()
+
+    pretty_config = json.dumps(config, indent=4, separators=(",", ": "))
+    return render(request, "config.html", {"config": pretty_config})
+
+
+@csrf_exempt
+def save_config(request):
+    user = get_user(request)
+    community = user.community
+    data = json.loads(request.body)
+
+    if data.get("name") != metagov_slug(community):
+        return HttpResponseBadRequest("Changing the name is not permitted")
+    if data.get("readable_name") != community.community_name:
+        return HttpResponseBadRequest("Changing the readable_name is not permitted")
+
+    logger.info(data.get("plugins"))
+    try:
+        update_metagov_community(community, data.get("plugins", []))
+    except Exception as e:
+        return HttpResponseBadRequest(e)
+
+    return HttpResponse()
+
+
 @csrf_exempt
 def post_outcome(request, id):
-    if request.method != 'POST' or not request.body:
+    if request.method != "POST" or not request.body:
         return HttpResponseBadRequest()
     try:
         process = ExternalProcess.objects.get(pk=id)
@@ -24,7 +79,7 @@ def post_outcome(request, id):
     logger.info(f"Received external process outcome: {body}")
     if body.get("status") != "completed":
         return HttpResponseBadRequest("process not completed")
-    if not body.get('outcome') and not body.get('errors'):
+    if not body.get("outcome") and not body.get("errors"):
         return HttpResponseBadRequest("completed process must have either outcome or errors")
 
     process.json_data = json.dumps(body)
@@ -34,12 +89,65 @@ def post_outcome(request, id):
 
 @csrf_exempt
 def action(request):
-    if request.method != 'POST' or not request.body:
+    """
+    Receive event from Metagov, and create a new MetagovPlatformAction.
+
+    1) Find the `Community` that this metagov community corresponds to (e.g. the SlackCommunity that was configured to use Metagov.)
+    2) Get or create a MetagovUser that' stied to the original community (the SlackCommunity)
+    3) Give the user permission to propose MetagovPlatformActions
+    """
+
+    if request.method != "POST" or not request.body:
         return HttpResponseBadRequest()
     try:
         body = json.loads(request.body)
     except ValueError:
         return HttpResponseBadRequest("unable to decode body")
-    logger.info(f"Received external action: {body}")
-    # TODO create ExternalPlatformAction
+    logger.info(f"Received metagov action: {body}")
+
+    metagov_community_slug = body.get("community")
+
+    try:
+        community = Community.objects.get_by_metagov_name(name=metagov_community_slug)
+    except Community.DoesNotExist:
+        logger.error(f"Received event for community {metagov_community_slug} which doesn't exist in PolicyKit")
+        return HttpResponseBadRequest("Community does not exist")
+
+    # Hack so MetagovUser username doesn't clash with usernames from other communities (django User requires unique username).
+    # TODO(#299): make the CommunityUser model unique on community+username, not just username.
+    initiator = body["initiator"]
+    prefixed_username = f"{initiator['provider']}.{initiator['user_id']}"
+    metagov_user, _ = MetagovUser.objects.get_or_create(
+        username=prefixed_username, provider=initiator["provider"], community=community
+    )
+
+    # Give this user permission to propose any MetagovPlatformAction
+    user_group, usergroup_created = CommunityRole.objects.get_or_create(
+        role_name="Base User", name=f"Metagov: {metagov_community_slug}: Base User"
+    )
+    if usergroup_created:
+        user_group.community = community
+        content_type = ContentType.objects.get_for_model(MetagovPlatformAction)
+        permission, _ = Permission.objects.get_or_create(
+            codename="add_metagovaction",
+            name="Can add metagov action",
+            content_type=content_type,
+        )
+        user_group.permissions.add(permission)
+        user_group.save()
+    user_group.user_set.add(metagov_user)
+
+    # Create MetagovPlatformAction
+    new_api_action = MetagovPlatformAction()
+    new_api_action.community = community
+    new_api_action.initiator = metagov_user
+    new_api_action.event_type = body["event_type"]
+    new_api_action.json_data = json.dumps(body["data"])
+
+    # Save to create Proposal and trigger PlatformPolicy evaluations
+    new_api_action.save()
+    if not new_api_action.pk:
+        return HttpResponseServerError()
+
+    logger.info(f"Created new MetagovPlatformAction with pk {new_api_action.pk}")
     return HttpResponse()
--- policykit/tests/test_policy_evaluation.py
+++ policykit/tests/test_policy_evaluation.py
@@ -1,35 +1,89 @@
 import json
+from unittest import skip
 
+import requests
 from django.contrib.auth.models import Permission
-from django.test import TestCase
-from integrations.metagov.models import ExternalProcess
-from integrations.slack.models import (SlackCommunity, SlackPinMessage,
-                                       SlackStarterKit, SlackUser)
-from policyengine.models import (CommunityRole, PlatformAction, PlatformPolicy,
-                                 Proposal)
-from policyengine.views import check_policy
+from django.test import Client, TestCase
+from integrations.metagov.library import update_metagov_community, metagov_slug
+from integrations.metagov.models import ExternalProcess, MetagovPlatformAction, MetagovUser
+from integrations.slack.models import SlackCommunity, SlackPinMessage, SlackStarterKit, SlackUser
+from policyengine.models import CommunityRole, PlatformAction, PlatformPolicy, Proposal
+from policyengine.views import check_policy, filter_policy
 
-"""
-FIXME: mock metagov service
-"""
+METAGOV_URL = "http://127.0.0.1:8000"
 
 
 class EvaluationTests(TestCase):
     def setUp(self):
-        user_group = CommunityRole.objects.create(
-            role_name="fake role", name="testing role")
+        # Set up a Slack community and a user
+        user_group = CommunityRole.objects.create(role_name="fake role", name="testing role")
         p1 = Permission.objects.get(name="Can add slack pin message")
         user_group.permissions.add(p1)
         self.community = SlackCommunity.objects.create(
-            community_name='test',
-            team_id='test',
-            bot_id='test',
-            access_token='test',
-            base_role=user_group)
-        self.user = SlackUser.objects.create(
-            username="test", community=self.community)
+            community_name="my test community",
+            team_id="TMQ3PKX",
+            bot_id="test",
+            access_token="test",
+            base_role=user_group,
+        )
+        self.user = SlackUser.objects.create(username="test", community=self.community)
 
-    def test_external_process(self):
+        # Activate a plugin to use in tests
+        update_metagov_community(
+            community=self.community,
+            plugins=list(
+                [
+                    {"name": "randomness", "config": {"default_low": 2, "default_high": 200}},
+                ]
+            ),
+        )
+
+    def test_close_process(self):
+        # 1) Create Policy and PlatformAction
+        policy = PlatformPolicy()
+        policy.community = self.community
+        policy.filter = "return True"
+        policy.initialize = """
+metagov.start_process("randomness.delayed-stochastic-vote", {"options": ["one", "two", "three"], "delay": 1})
+"""
+        policy.notify = ""
+        policy.check = """
+result = metagov.close_process()
+action.data.set('status', result.status)
+action.data.set('outcome', result.outcome)
+
+if result is None:
+    return #still processing
+if result.errors:
+    return FAILED
+if result.outcome:
+    return PASSED if result.outcome.get('winner') else FAILED
+return FAILED
+"""
+        policy.success = "pass"
+        policy.fail = "pass"
+        policy.description = "test"
+        policy.name = "test policy"
+        policy.save()
+
+        action = SlackPinMessage()
+        action.initiator = self.user
+        action.community = self.community
+
+        # 2) Save action to trigger policy execution
+        action.save()
+
+        process = ExternalProcess.objects.filter(action=action, policy=policy).first()
+        self.assertIsNotNone(process)
+
+        # Fails because of bug https://github.com/amyxzhang/policykit/issues/305
+        # self.assertEqual(action.proposal.status, "passed")
+
+        self.assertEqual(action.data.get("status"), "completed")
+        self.assertIsNotNone(action.data.get("outcome").get("winner"))
+
+    @skip("Don't run loomio vote test because it requires an API key")
+    def test_loomio_vote(self):
         print("\nTesting external process\n")
         # 1) Create Policy and PlatformAction
         policy = PlatformPolicy()
@@ -37,7 +91,7 @@ class EvaluationTests(TestCase):
         policy.filter = "return True"
         policy.initialize = "pass"
         policy.notify = """
-result = metagov.start_process("loomio", {"closing_at": "2021-05-11"})
+result = metagov.start_process("loomio.poll", {"title": "[test] policykit poll", "options": ["one", "two", "three"], "closing_at": "2021-05-11"})
 poll_url = result.get('poll_url')
 action.data.set('poll_url', poll_url)
 """
@@ -64,26 +118,24 @@ return FAILED
         # 2) Save action to trigger execution of check() and notify()
         action.save()
 
-        process = ExternalProcess.objects.filter(
-            action=action, policy=policy).first()
+        process = ExternalProcess.objects.filter(action=action, policy=policy).first()
         self.assertIsNotNone(process)
         self.assertEqual(process.json_data, None)
         self.assertEqual(action.proposal.status, "proposed")
-        self.assertTrue(
-            'https://www.loomio.org/p/' in action.data.get('poll_url'))
+        self.assertTrue("https://www.loomio.org/p/" in action.data.get("poll_url"))
 
         # 3) Invoke callback URL to notify PolicyKit that process is complete
         # FIXME make this endpoint idempotent
         # FIXME used stored callback url instead of assuming
-        from django.test import Client
+
         client = Client()
         data = {
-            'status': 'completed',
+            "status": "completed",
             # 'errors': {'text': 'something went wrong'}
-            'outcome': {'value': 27}
+            "outcome": {"value": 27},
         }
 
-        response = client.post(f"/metagov/outcome/{process.pk}", data=data, content_type='application/json')
+        response = client.post(f"/metagov/outcome/{process.pk}", data=data, content_type="application/json")
         self.assertEqual(response.status_code, 200)
 
         process.refresh_from_db()
@@ -94,31 +146,83 @@ return FAILED
         self.assertEqual(result_obj, data)
 
         result = check_policy(policy, action)
-        self.assertEqual(result, 'passed')
+        self.assertEqual(result, "passed")
 
-    def test_get_resource(self):
-        print("\nTesting get_resource from metagov\n")
-        # 1) Create Policy and PlatformAction
+    def test_perform_action(self):
+        print("\nTesting perform_action from metagov\n")
+        # 1) Create Policy that performs a metagov action
         policy = PlatformPolicy()
         policy.community = self.community
         policy.filter = "return True"
         policy.initialize = "pass"
-        policy.check = """response = metagov.get_resource('cred', { 'username': 'miriam' })\n
-cred = response.get('value')\n
-return PASSED if cred > 0.5 else FAILED"""
-        policy.notify = """pass"""
-        policy.success = "pass"
+        policy.check = """parameters = {"low": 4, "high": 5}
+response = metagov.perform_action('randomness.random-int', parameters)
+if response and response.get('value') == 4:
+    return PASSED
+return FAILED"""
+        policy.notify = "pass"
+        policy.success = "action.execute()"  # Needed to mark as "passed" because of bug https://github.com/amyxzhang/policykit/issues/305
         policy.fail = "pass"
         policy.description = "test"
         policy.name = "test policy"
         policy.save()
 
+        # 2) Save an action to trigger the policy execution
         action = SlackPinMessage()
         action.initiator = self.user
         action.community = self.community
-
-        # 2) Save action to trigger execution of check() and notify()
         action.save()
 
-        result = check_policy(policy, action)
-        self.assertEqual(result, 'passed')
+        self.assertEqual(action.proposal.status, "passed")
+
+
+class MetagovPlatformActionTest(TestCase):
+    def setUp(self):
+        user_group = CommunityRole.objects.create(role_name="fake role", name="testing role")
+        p1 = Permission.objects.get(name="Can add slack pin message")
+        user_group.permissions.add(p1)
+        self.community = SlackCommunity.objects.create(
+            community_name="test community", team_id="test123", bot_id="test", access_token="test", base_role=user_group
+        )
+
+    def test_metagov_trigger(self):
+        # 1) Create Policy that is triggered by a metagov action
+
+        policy = PlatformPolicy()
+        policy.community = self.community
+        policy.filter = """return action.action_codename == 'metagovaction' \
+and action.event_type == 'discourse_post_created'"""
+        policy.initialize = "action.data.set('test_verify_username', action.initiator.metagovuser.get_real_username())"
+        policy.notify = "pass"
+        policy.check = "return PASSED if action.get_metagov_action_data()['category'] == 0 else FAILED"
+        policy.success = "action.execute()"  # Needed to mark as "passed" because of bug https://github.com/amyxzhang/policykit/issues/305
+        policy.fail = "pass"
+        policy.description = "test"
+        policy.name = "test policy"
+        policy.save()
+
+
+        event_payload = {
+            "community": metagov_slug(self.community),
+            "initiator": {"user_id": "miriam", "provider": "discourse"},
+            "event_type": "discourse_post_created",
+            "data": {"title": "test", "raw": "post text", "category": 0},
+        }
+
+        # 2) Mimick an incoming notification from Metagov that the action has occurred
+        client = Client()
+        response = client.post(f"/metagov/action", data=event_payload, content_type="application/json")
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(MetagovPlatformAction.objects.all().count(), 1)
+
+        action = MetagovPlatformAction.objects.filter(event_type="discourse_post_created").first()
+
+        self.assertEqual(action.community.platform, "slack") ## the action.community is the community that is connected to metagov
+        self.assertEqual(action.initiator.username, "discourse.miriam")
+        self.assertEqual(action.initiator.metagovuser.get_real_username(), "miriam")
+        self.assertEqual(action.data.get("test_verify_username"), "miriam")
+        self.assertEqual(action.get_metagov_action_data()["raw"], "post text")
+
+        self.assertEqual(action.proposal.status, "passed")
