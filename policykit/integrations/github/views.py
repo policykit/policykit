0dd657ae4b98cb87d1370a3d8776ed94524150d2
Miriam Ashton <miriamashton3@gmail.com>
2021-08-11 11:34:08 -0400

    clean up view

--- policykit/integrations/github/views.py
+++ policykit/integrations/github/views.py
@@ -2,7 +2,7 @@ import logging
 import requests
 from django.conf import settings
 from django.shortcuts import redirect
-from policyengine.models import Community, CommunityRole
+from policyengine.models import Community
 
 logger = logging.getLogger(__name__)
 
@@ -12,17 +12,15 @@ def github_install(request):
 
     # metagov identifier for the "parent community" to install GitHub to
     metagov_community_slug = request.GET.get("community")
-    is_new_community = False
+    # we can only add GitHub to existing communities for now, so redirect to the settings page
+    redirect_route = "/main/settings"
+
     try:
-        community = Community.objects.get(metagov_slug=metagov_community_slug)
+        Community.objects.get(metagov_slug=metagov_community_slug)
     except Community.DoesNotExist:
-        logger.debug(f"Community not found: {metagov_community_slug}, creating it")
-        is_new_community = True
-        community = Community.objects.create(metagov_slug=metagov_community_slug)
+        return redirect(f"{redirect_route}?error=community_not_found")
 
-    # if we're enabling an integration for an existing community, so redirect to the settings page
-    redirect_route = "/login" if is_new_community else "/main/settings"
-
+    # validate state
     expected_state = request.session.get("community_install_state")
     if expected_state is None or request.GET.get("state") is None or (not request.GET.get("state") == expected_state):
         logger.error(f"expected {expected_state}")
