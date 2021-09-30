5f1b905297af8b490c0e43eceeb373abdaa8cc51
Miriam Ashton <miriamashton3@gmail.com>
2021-09-30 12:27:22 -0400

    Add function for user lookup by platform identifier

--- policykit/integrations/metagov/api.py
+++ policykit/integrations/metagov/api.py
@@ -24,7 +24,6 @@ def create_empty_metagov_community(readable_name=""):
     return response.json()
 
 
-
 def update_metagov_community(community, plugins=[]):
     if not community.metagov_slug:
         raise Exception(f"no metagov slug for {community}")
@@ -42,8 +41,34 @@ def get_metagov_community(slug):
         raise Exception(response.text or "Unknown error")
     return response.json()
 
+
+#### IDENTITY MANAGEMENT ####
+
+
+def find_metagov_id(platform_type, community_platform_id, platform_identifier):
+    logger.debug(f">> user lookup for {platform_identifier} on {platform_type}:{community_platform_id}")
+    response = requests.get(
+        f"{settings.METAGOV_URL}/api/internal/users",
+        params={
+            "platform_type": platform_type,
+            "community_platform_id": community_platform_id,
+            "platform_identifier": platform_identifier,
+        },
+    )
+    if not response.ok:
+        raise Exception(response.text or "Unknown error")
+    users = response.json()
+    logger.debug(f">> Metagov user request returned {users}")
+    if not users:
+        return None
+    if len(users) > 1:
+        raise Exception
+    return users[0]["source_ID"]
+
+
 #### PLUGIN MANAGEMENT ####
 plugin_base = f"{settings.METAGOV_URL}/api/internal/plugin"
+
 
 def enable_plugin(community_slug, name, config):
     headers = {"X-Metagov-Community": community_slug}
@@ -52,12 +77,15 @@ def enable_plugin(community_slug, name, config):
         raise Exception(response.text or "Unknown error")
     return response.json()
 
+
 def delete_plugin(name: str, id):
     response = requests.delete(f"{plugin_base}/{name}/{id}")
     if not response.ok and response.status_code != 404:
         raise Exception(response.text or "Unknown error")
 
+
 #### SCHEMAS ####
+
 
 def get_plugin_config_schemas():
     url = f"{settings.METAGOV_URL}/api/internal/plugin-schemas"
@@ -65,6 +93,7 @@ def get_plugin_config_schemas():
     if not response.ok:
         raise Exception(response.text or "Unknown error")
     return response.json()
+
 
 def get_plugin_metadata(plugin):
     url = f"{settings.METAGOV_URL}/api/internal/plugin/{plugin}/metadata"
