48b5880b0247071be909105bd1751e4d4dd30c54
Miriam Ashton <miriamashton3@gmail.com>
2021-12-14 14:42:50 -0500

    register handlers

--- policykit/integrations/discord/apps.py
+++ policykit/integrations/discord/apps.py
@@ -4,5 +4,7 @@ class discordIntegrationConfig(AppConfig):
     name = 'integrations.discord'
 
     def ready(self):
+        import integrations.discord.handlers
+
         from actstream import registry
         registry.register(self.get_model('DiscordUser'))
