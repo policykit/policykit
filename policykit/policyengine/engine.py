163ca1b323d66995793bea5d6c18a50bf492382b
Miriam Ashton <miriamashton3@gmail.com>
2021-12-29 16:59:51 -0500

    Wip

--- policykit/policyengine/engine.py
+++ policykit/policyengine/engine.py
@@ -273,19 +273,17 @@ def evaluate_proposal_inner(context: EvaluationContext, is_first_evaluation: boo
         # logger.debug("does not pass filter")
         raise PolicyDoesNotPassFilter
 
-    # If policy is being evaluated for the first time, initialize it
+    # If policy is being evaluated for the first time, run "initialize" block
     if is_first_evaluation:
-        context.logger.debug(f"Evaluating {proposal}")
-        # run "initialize" block of policy
         exec_code_block(policy.initialize, context, Policy.INITIALIZE)
 
     # Run "check" block of policy
     check_result = exec_code_block(policy.check, context, Policy.CHECK)
-    check_result = sanitize_check_result(check_result)
+    check_result = sanitize_check_result(check_result) # sanitize so None becomes PROPOSED
 
     if is_first_evaluation or check_result != Proposal.PROPOSED:
         # log the check result for first evaluation, or if the proposal is newly completed
-        context.logger.debug(f"Proposal {proposal.pk} check returned '{check_result.upper()}'")
+        context.logger.debug(f"Evaluating Proposal {proposal.pk}, check returned {check_result.upper()}")
 
     if check_result == Proposal.PASSED:
         # run "pass" block of policy
