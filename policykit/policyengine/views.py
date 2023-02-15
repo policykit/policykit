33f8eb4fba96127f55af4d7dba70ce5329300a8b
Leijie Wang <leijiew@cs.washington.edu>
2023-02-15 09:49:46 -0800

    remove expenses templates

--- policykit/policyengine/views.py
+++ policykit/policyengine/views.py
@@ -854,39 +854,6 @@ def embed_populate_templates(request):
     with loading from an endpoint, uploading JSON, etc.
     """
     from policyengine.models import Policy, PolicyVariable, ActionType
-
-    desc = """
-    Posts OpenCollective expenses to Slack channel to be voted on. 
-    You can set a minimum number of yes votes and maximum number of no votes needed to pass.
-    After a three hour voting window, expense will be approved if enough yes votes and few enough no votes were cast.
-    Once the vote is resolved, posts to both Slack thread and the OpenCollective expense thread with vote results. Restricts voting on OpenCollective expenses to eligible voters.
-    """
-    policy, created = Policy.objects.get_or_create(
-        kind="trigger",
-        name="Expense Voting",
-        filter="return True",
-        initialize="pass",
-        check="if not proposal.vote_post_id:\n  return None\n\nyes_votes = proposal.get_yes_votes().count()\nno_votes = proposal.get_no_votes().count()\ntime_elapsed = proposal.get_time_elapsed()\nif time_elapsed < datetime.timedelta(hours=3):\n  return None  \n\nif no_votes >= variables[\"no_votes_to_reject\"]:\n  return FAILED\n\nif yes_votes >= variables[\"yes_votes_to_approve\"]:\n  return PASSED\n\nreturn PROPOSED # still pending",
-        notify="discussion_channel = variables[\"slack_channel_id\"]\n\nmessage = f\"Vote on whether to approve <{action.url}|this request> for funds: {action.description}\"\nslack.initiate_vote(text=message, channel=discussion_channel)\n\n# Start a discussion thread on the voting message\nslack.post_message(text=\"Discuss here, if needed.\", channel=discussion_channel, thread_ts=proposal.vote_post_id)\n\n# Add a comment to the expense on Open Collective with a link to the Slack vote\nlink = f\"<a href='{proposal.vote_url}'>on Slack</a>\"\ntext = f\"Thank you for submitting a request! A vote has been started {link}.\"\nopencollective.post_message(text=text, expense_id=action.expense_id)\n",
-        success="# approve the expense\nopencollective.process_expense(action=\"APPROVE\", expense_id=action.expense_id)\n\nyes_votes = proposal.get_yes_votes().count()\nno_votes = proposal.get_no_votes().count()\nmessage = f\"Expense approved. The vote passed with {yes_votes} for and {no_votes} against.\"\n\n# comment on the expense\nopencollective.post_message(text=message, expense_id=action.expense_id)\n\n# update the Slack thread\nslack.post_message(text=message, channel=variables[\"slack_channel_id\"], thread_ts=proposal.vote_post_id)\n",
-        fail="# reject the expense\nopencollective.process_expense(action=\"REJECT\", expense_id=action.expense_id)\n\nyes_votes = proposal.get_yes_votes().count()\nno_votes = proposal.get_no_votes().count()\nmessage = f\"Expense rejected. The vote failed with {yes_votes} for and {no_votes} against.\"\n\n# comment on the expense\nopencollective.post_message(text=message, expense_id=action.expense_id)\n\n# update the Slack thread\nslack.post_message(text=message, channel=variables[\"slack_channel_id\"], thread_ts=proposal.vote_post_id)\n",
-        is_template=True,
-        description=desc
-    )
-    if created:
-        action_type, _ = ActionType.objects.get_or_create(codename="expensecreated")
-        policy.action_types.add(action_type)
-
-        PolicyVariable.objects.create(
-            name="yes_votes_to_approve", label="Yes Votes Needed To Approve", default_value=1, is_required=True,
-            prompt="If this number of YES votes is hit, the expense is approved", type="number", policy=policy)
-        PolicyVariable.objects.create(
-            name="no_votes_to_reject", label="No Votes Needed to Reject", default_value=1, is_required=True,
-            prompt="If this number of NO votes is hit, the expense is rejected", type="number", policy=policy)
-        PolicyVariable.objects.create(
-            name="slack_channel_id", label="Slack Channel ID", default_value="", is_required=True,
-            prompt="Which Slach Channel to use?", type="string", policy=policy)
-        
     
     desc = """
     For testing: add a very simple policy so that when you post "ping" in Slack,
