4734d6b3471c254896c85887a09fce4bb0bd3a8e
Leijie Wang <leijiew@cs.washington.edu>
2023-02-20 09:28:22 -0800

    Add deleting templates function

--- policykit/policykit/urls.py
+++ policykit/policykit/urls.py
@@ -50,6 +50,7 @@ urlpatterns = [
     path('main/logs/', include('django_db_logger.urls', namespace='django_db_logger')),
     
     path('embed/populate_templates', policyviews.embed_populate_templates),
+    path('embed/delete_templates', policyviews.embed_delete_templates),
     path('embed/select_template', policyviews.embed_select_template),
     path('embed/', policyviews.embed_initial),
     path('embed/setup', policyviews.embed_setup),
