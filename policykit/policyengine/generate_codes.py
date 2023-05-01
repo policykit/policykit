d21946d75d09a2f68240ceb12c8f667ea61e7d05
Leijie Wang <leijiew@cs.washington.edu>
2023-04-30 21:33:55 -0700

    Allow users to use f-strings when specifying execution fields (such as the vote_message of slack.initiate_vote); however, we only allow users to embed variables.name, action.name, or proposal.name codes for security reasons

--- policykit/policyengine/generate_codes.py
+++ policykit/policyengine/generate_codes.py
@@ -1,3 +1,47 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+def check_format_string(string):
+    """ 
+        Check whether the string contains any embedded variables or data, and format it accordingly 
+        TODO: check whether the referenced variables or data are defined
+    """
+    import re
+    curley_pattern = r"\{(.+?)\}"
+    variable_pattern = r"variables\.([a-zA-Z_][a-zA-Z0-9_]*)"
+    action_pattern = r"action\.([a-zA-Z_][a-zA-Z0-9_]*)"
+    proposal_pattern = r"proposal\.([a-zA-Z_][a-zA-Z0-9_]*)"
+
+    required_f_string = False
+    for match in re.finditer(curley_pattern, string):
+        match_str = match.group(0)
+        content = match.group(1)
+        logger.debug(f"Matched string: {match_str}, content {content}")
+        
+        variable_match = re.match(variable_pattern, content)
+        if variable_match:
+            if variable_match.group(1).isidentifier():
+                required_f_string = True
+            else:
+                logger.warning(f"Embedded codes in a f-string {match_str} is not a valid identifier")
+        
+        action_match = re.match(action_pattern, content)
+        if action_match:
+            if action_match.group(1).isidentifier():
+                required_f_string = True
+            else:
+                logger.warning(f"Embedded codes in a f-string {match_str} is not a valid identifier")
+        
+        proposal_match = re.match(proposal_pattern, content)
+        if proposal_match:
+            if proposal_match.group(1).isidentifier():
+                required_f_string = True
+            else:
+                logger.warning(f"Embedded codes in a f-string {match_str} is not a valid identifier")
+                
+    return string, required_f_string
+
 def force_variable_types(value, variable):
     """
         when generating codes, we need to make sure the value specified by users (a string) are correctly embedded in the codes
@@ -38,7 +82,8 @@ def force_variable_types(value, variable):
                 # an additional f is included so that variables inside the string can be evaluated
                 
                 # add safety check to make sure the string does not contain any malicious codes
-                value_codes = f"\"{value}\""
+                value, required_f_string = check_format_string(value)
+                value_codes = f"f\"{value}\"" if required_f_string else f"\"{value}\""
             else:
                 raise NotImplementedError
     return value_codes
