54fc94060bf1ccb54ded07c05c06c101652063ad
Leijie Wang <leijiew@cs.washington.edu>
2023-05-19 21:11:41 -0700

    Add code for generating the filter code block

--- policykit/policyengine/generate_codes.py
+++ policykit/policyengine/generate_codes.py
@@ -44,3 +44,93 @@ def extract_action_types(filters):
         if action_type:
             action_types.append(action_type)
     return action_types
+
+def generate_filter_codes(filters):
+    """
+        Generate codes from a list of filters defined in JSON
+        See examples of the parameter filters above 
+
+        The generated codes will be in the shape of 
+        if action.action_type == "slackpostmessage":
+	        def CommunityUser_Role(role, object=None):
+		        all_usernames_with_roles = [_user.username for _user in slack.get_users(role_names=[role])]
+		        return (object.username in all_usernames_with_roles) if object else None, all_usernames_with_roles
+	        def Text_Equals(text, object=None):
+		        return object == text, None
+	        return CommunityUser_Role("test", action.initiator)[0] and Text_Equals("test", action.text)[0]
+
+    """
+
+    from policyengine.models import FilterModule
+
+    filter_codes = ""
+    for action_filter in filters:
+        # we first check whether the action is the one we want to apply filters to
+        filter_codes += "if action.action_type == \"{action_type}\":\n\t".format(action_type = action_filter["action_type"])
+        # one example: "if action.action_type == \"slackpostmessage\":\n\t
+        
+        now_codes = []
+        function_calls = [] # a list of names of filter functions we will call in the end for each action type
+        
+        # only custom actions have the filter key
+        for field, field_filter in action_filter.get("filter", {}).items():
+            """  e.g.,
+                    "initiator": {
+                        "kind": "CommunityUser",
+                        "name": "Role",
+                        "variables": [
+                            {
+                                "name": "role",
+                                "label": "Which role users should have?",
+                                "entity": "Role",
+                                "default_value": null,
+                                "is_required": true,
+                                "prompt": "",
+                                "type": "string",
+                                "is_list": false
+                            }
+                        ],
+                        "platform": "slack"
+                    },
+            """
+            if field_filter:
+                filter = FilterModule.objects.filter(kind=field_filter["kind"], name=field_filter["name"]).first()
+                if not filter:
+                    raise Exception(f"Filter {field_filter['kind']}_{field_filter['name']} not found")
+                
+                field_filter["codes"] = filter.codes
+                parameters_codes = "object, " + ", ".join([var["name"]  for var in field_filter["variables"]])
+                now_codes.append(
+                    "def {kind}_{name}({parameters}):".format(
+                        kind = field_filter["kind"], 
+                        name = field_filter["name"],
+                        parameters = parameters_codes
+                    )
+                ) # result example: def CommunityUser_Role(role, object=None):
+
+
+                module_codes = field_filter["codes"].format(platform=field_filter["platform"])
+                # in case the exact platform such as slack is used in the codes
+                module_codes = ["\t" + line for line in module_codes.splitlines()]
+                # because these codes are put inside a function, we need to indent them
+
+                now_codes.extend(module_codes)
+
+                parameters_called = []
+                parameters_called.append("action.{field}".format(field=field)) # action.initiator
+                for var in field_filter["variables"]:
+                    # we need to make sure the value specified by users (a string) are correctly embedded in the codes
+                    parameters_called.append(force_variable_types(var["value"], var))
+                parameters_called = ", ".join(parameters_called) # action.initiator, "test"
+                function_calls.append(
+                    "{kind}_{name}({parameters})".format(
+                        kind = field_filter["kind"], 
+                        name = field_filter["name"], 
+                        parameters = parameters_called
+                    )
+                )
+        if now_codes:
+            filter_codes += "\n\t".join(now_codes) + "\n\treturn " + " and ".join(function_calls) + "\n"
+        else:
+            filter_codes += "return True\n"
+    return filter_codes
