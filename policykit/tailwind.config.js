1af4f43d3e078cbb5bd4847b91e2d2c0ed8f6c03
Jack MB <jack@designandcode.com>
2023-10-20 15:04:41 +0200

    fix link hover colours and menu backdrop

--- policykit/tailwind.config.js
+++ policykit/tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
       background: {
         light: "#F8F9FA",
         focus: "#F1F4F6",
-        popup: "#384450",
+        popup: "#667C93",
       },
       grey: {
         darkest: "#191F24",
