dc4da05160ab669d0cee19796642611e7892586c
Leijie Wang <leijiew@cs.washington.edu>
2024-11-19 15:27:52 -0800

    Restore old migration files.

--- policykit/integrations/discord/migrations/0002_discordcommunity_discordcreatechannel_discorddeletechannel_discorddeletemessage_discordpostmessage_d.py
+++ policykit/integrations/discord/migrations/0002_discordcommunity_discordcreatechannel_discorddeletechannel_discorddeletemessage_discordpostmessage_d.py
@@ -10,12 +10,13 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
+        ('discord', '0001_initial'),
         ('policyengine', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='SlackCommunity',
+            name='DiscordCommunity',
             fields=[
                 ('communityplatform_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='policyengine.communityplatform')),
                 ('team_id', models.CharField(max_length=150, unique=True, verbose_name='team_id')),
@@ -27,82 +28,69 @@ class Migration(migrations.Migration):
             bases=('policyengine.communityplatform',),
         ),
         migrations.CreateModel(
-            name='SlackJoinConversation',
+            name='DiscordCreateChannel',
             fields=[
                 ('governableaction_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='policyengine.governableaction')),
-                ('channel', models.CharField(max_length=150, verbose_name='channel')),
-                ('users', models.CharField(max_length=15, verbose_name='users')),
+                ('channel_id', models.BigIntegerField(blank=True)),
+                ('name', models.TextField()),
             ],
             options={
-                'permissions': (('can_execute_slackjoinconversation', 'Can execute slack join conversation'),),
+                'permissions': (('can_execute_discordcreatechannel', 'Can execute discord create channel'),),
             },
             bases=('policyengine.governableaction',),
         ),
         migrations.CreateModel(
-            name='SlackKickConversation',
+            name='DiscordDeleteChannel',
             fields=[
                 ('governableaction_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='policyengine.governableaction')),
-                ('user', models.CharField(max_length=15, verbose_name='user')),
-                ('channel', models.CharField(max_length=150, verbose_name='channel')),
+                ('channel_id', models.BigIntegerField()),
             ],
             options={
-                'permissions': (('can_execute_slackkickconversation', 'Can execute slack kick conversation'),),
+                'permissions': (('can_execute_discorddeletechannel', 'Can execute discord delete channel'),),
             },
             bases=('policyengine.governableaction',),
         ),
         migrations.CreateModel(
-            name='SlackPinMessage',
+            name='DiscordDeleteMessage',
             fields=[
                 ('governableaction_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='policyengine.governableaction')),
-                ('channel', models.CharField(max_length=150, verbose_name='channel')),
-                ('timestamp', models.CharField(max_length=32)),
+                ('channel_id', models.BigIntegerField()),
+                ('message_id', models.BigIntegerField()),
+                ('text', models.TextField(blank=True, default='')),
             ],
             options={
-                'permissions': (('can_execute_slackpinmessage', 'Can execute slack pin message'),),
+                'permissions': (('can_execute_discorddeletemessage', 'Can execute discord delete message'),),
             },
             bases=('policyengine.governableaction',),
         ),
         migrations.CreateModel(
-            name='SlackPostMessage',
+            name='DiscordPostMessage',
             fields=[
                 ('governableaction_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='policyengine.governableaction')),
+                ('channel_id', models.BigIntegerField()),
+                ('message_id', models.BigIntegerField()),
                 ('text', models.TextField()),
-                ('channel', models.CharField(max_length=150, verbose_name='channel')),
-                ('timestamp', models.CharField(blank=True, max_length=32)),
             ],
             options={
-                'permissions': (('can_execute_slackpostmessage', 'Can execute slack post message'),),
+                'permissions': (('can_execute_discordpostmessage', 'Can execute discord post message'),),
             },
             bases=('policyengine.governableaction',),
         ),
         migrations.CreateModel(
-            name='SlackRenameConversation',
+            name='DiscordRenameChannel',
             fields=[
                 ('governableaction_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='policyengine.governableaction')),
-                ('name', models.CharField(max_length=150, verbose_name='name')),
-                ('channel', models.CharField(max_length=150, verbose_name='channel')),
-                ('previous_name', models.CharField(max_length=80)),
+                ('channel_id', models.BigIntegerField()),
+                ('name', models.TextField()),
+                ('name_old', models.TextField(blank=True, default='')),
             ],
             options={
-                'permissions': (('can_execute_slackrenameconversation', 'Can execute slack rename conversation'),),
+                'permissions': (('can_execute_discordrenamechannel', 'Can execute discord rename channel'),),
             },
             bases=('policyengine.governableaction',),
         ),
         migrations.CreateModel(
-            name='SlackScheduleMessage',
-            fields=[
-                ('governableaction_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='policyengine.governableaction')),
-                ('text', models.TextField()),
-                ('channel', models.CharField(max_length=150, verbose_name='channel')),
-                ('post_at', models.IntegerField(verbose_name='post at')),
-            ],
-            options={
-                'permissions': (('can_execute_slackschedulemessage', 'Can execute slack schedule message'),),
-            },
-            bases=('policyengine.governableaction',),
-        ),
-        migrations.CreateModel(
-            name='SlackUser',
+            name='DiscordUser',
             fields=[
                 ('communityuser_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='policyengine.communityuser')),
             ],
